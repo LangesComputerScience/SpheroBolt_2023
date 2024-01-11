9f950194f39e0279b922a84c322cbdaab59c84bc
Lange <langescomputerscience@gmail.com>
2024-01-11 08:13:00 -0500

    Hello World connected and stable

--- Sandbox.js
+++ Sandbox.js
@@ -1,3 +1,12 @@
 //Programmer: Mr. Lange
 //Date: 1.10.2024
 //Program: Sphero Bolt Testing
+//Bolt ID: 6B78
+
+async function startProgram() {
+	await speak("Hello World", true);
+	setMainLed({ r: 0, g: 0, b: 255});
+	setSpeed(60);
+	await delay (2);
+	setSpeed(0);
+}
